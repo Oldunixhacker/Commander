b52b69410e2e10bcc62dcfad5d7998df04eb7270
Tyler887 <tylermageeshields@gmail.com>
2022-05-28 19:48:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+Console.WriteLine("Welcome to Windows Commander 1.0.");
+
+while (true)
+{
+    Console.Write("Commander> ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+    var command = Console.ReadLine();
+    System.Diagnostics.ProcessStartInfo proc = new System.Diagnostics.ProcessStartInfo();
+    proc.FileName = @"C:\windows\system32\cmd.exe";
+    proc.Arguments = $"/c {command}";
+    if ( command == "exit" ) {
+        return 0;
+    }
+    System.Diagnostics.Process.Start(proc).WaitForExit();
+}
